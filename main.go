0f15d81e4ace3f0ab42cb3cf61f08197a39d5cd6
Yannick Meeus <yannick.meeus@gmail.com>
2021-10-18 09:57:53 +0300

    Call a pokemon api

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	response, err := http.Get("http://pokeapi.co/api/v2/pokedex/kanto/")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var bunchOfPokemon BunchOfPokemon
+	json.Unmarshal(responseData, &bunchOfPokemon)
+
+	for _, pokemon := range bunchOfPokemon.Pokemon {
+		fmt.Println(pokemon.Species.Name)
+	}
 }
